332621dce8e7796a3897578530f247b311c0cc54
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-04-21 18:30:31 -0300

    Seção 3: Testes de ponta a ponta > 11. Meu primeiro teste automatizado

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,7 @@
+describe('home', () => {
+  it('webapp deve estar online', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.title().should('eq', 'Gerencie suas tarefas com Mark L')
+  })
+})
