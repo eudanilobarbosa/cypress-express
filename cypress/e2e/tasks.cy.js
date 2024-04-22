c1b459e290fdb3b8b7cf549dbbed20cfdd63c811
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-04-22 15:08:18 -0300

    Seção 4: Aprimorando os testes > 18. Testes independentes

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -3,7 +3,6 @@
 describe('tarefas', () => {
 
     it('deve cadastrar uma nova tarefa', () => {
-
         cy.request({
             url: 'http://localhost:3333/helper/tasks',
             method: 'DELETE',
@@ -21,6 +20,40 @@ describe('tarefas', () => {
 
         cy.contains('main div p', 'Ler um livro de Node.js')
             .should('be.visible')
+    })
+
+    it('não deve permitir tarefa duplicada', () => {
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: 'Estudar Javascript' }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        // Dado  que eu tenho uma tarefa duplicada
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: { name: 'Estudar Javascript', is_done: false }
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })       
+
+        // Quando faço o cadastro dessa tarefa
+        cy.visit('http://localhost:3000')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Estudar Javascript')
+
+        cy.contains('button', 'Create').click()
+
+        // Então vejo a mensagem de duplicidade
+
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
     })
 
 })
