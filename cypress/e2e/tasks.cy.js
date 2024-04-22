69b901173b03b19ea2fbd69f17aa32513bff29e4
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-04-22 14:34:42 -0300

    Seção 3: Testes de ponta a ponta > 15. Consumindo a API Rest

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,14 +1,21 @@
 /// <reference types="cypress" />
 
-import { faker } from '@faker-js/faker'
-
 describe('tarefas', () => {
 
     it('deve cadastrar uma nova tarefa', () => {
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: 'Ler um livro de Node.js' }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
         cy.visit('http://localhost:3000')
 
         cy.get('input[placeholder="Add a new Task"]')
-            .type(faker.music.songName())
+            .type('Ler um livro de Node.js')
 
         cy.contains('button', 'Create').click()
     })
